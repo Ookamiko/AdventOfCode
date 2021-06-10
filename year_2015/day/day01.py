67a1bb8fbc4e8e110af42963efb37ab763f68f1b
Valentin Dewilde <valentind@dufour.be>
2021-06-10 16:44:39 +0200

    [ADD] Resolve stage 2015-1-1 and 2015-1-2

--- year_2015/day/day01.py
+++ year_2015/day/day01.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python
 
 def level1(input):
-	return "Not implemented"
+	str_list = list(input)
+	return str(str_list.count('(') - str_list.count(')'))
 
 def level2(input):
-	return "Not implemented"
+	str_list = list(input)
+	count = 0
+	index = 0
+	for i in range(len(str_list)):
+		count += 1 if str_list[i] == '(' else -1
+		if count < 0:
+			index = i
+			break;
+
+	return str(index + 1)
