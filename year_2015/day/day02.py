eeeb46f8c936f6e2b3da592b65bd32afb4664ecb
Valentin Dewilde <valentind@dufour.be>
2021-06-11 11:00:04 +0200

    [ADD] Resolve stage 2015-2-1 and 2015-2-2

--- year_2015/day/day02.py
+++ year_2015/day/day02.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python
 
+import array
+
 def level1(input):
-	return "Not implemented"
+	lines = input.split('\n')
+	result = 0
+	for line in lines:
+		if len(line) == 0:
+			continue
+		list_obj = line.split('x')
+		size = sorted([int(list_obj[0]), int(list_obj[1]), int(list_obj[2])])
+		result += 2 * (size[0] * size[1] + size[1] * size[2] + size[0] * size[2]) + size[0] * size[1]
+	return str(result)
 
 def level2(input):
-	return "Not implemented"
+	lines = input.split('\n')
+	result = 0
+	for line in lines:
+		if len(line) == 0:
+			continue
+		list_obj = line.split('x')
+		size = sorted([int(list_obj[0]), int(list_obj[1]), int(list_obj[2])])
+		result += 2 * (size[0] + size[1]) + size[0] * size[1] * size[2]
+	return str(result)
