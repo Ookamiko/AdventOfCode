50ee68f9cc7ad6231fbbf2b254b42c5864ddd4c0
Valentin Dewilde <valentind@dufour.be>
2021-11-25 15:53:25 +0100

    [ADD] Resolve stage 2015-8-1 and 2015-8-2

--- year_2015/day/day08.py
+++ year_2015/day/day08.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python
+import re as regex
 
 def level1(input):
-	return "Not implemented"
+	str_list = input.strip().split("\n")
+	count_total = 0
+	count_code = 0
+
+	for current in str_list:
+		count_total += len(current)
+		count_code += len(regex.sub(r'(\\\\)|(\\")|(\\x[a-f0-9]{2})', "a", current).replace("\"", ""))
+
+	return str(count_total - count_code)
 
 def level2(input):
-	return "Not implemented"
+	str_list = input.strip().split("\n")
+	count_new = 0
+	count_old = 0
+
+	for current in str_list:
+		count_old += len(current)
+		count_new += len(current.replace('\\', '\\\\').replace('"', '\\"')) + 2
+
+	return str(count_new - count_old)
