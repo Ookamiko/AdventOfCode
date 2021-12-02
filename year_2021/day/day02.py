7b8ae2a94d67856d232c7a82687f9c6c1c450faf
Valentin Dewilde <valentind@dufour.be>
2021-12-02 08:50:22 +0100

    [ADD] Resolve stage 2021-2-1 and 2021-2-2

--- year_2021/day/day02.py
+++ year_2021/day/day02.py
@@ -1,7 +1,35 @@
 #!/usr/bin/python
 
 def level1(input):
-	return "Not implemented"
+	instructions = input.strip().split('\n')
+	position = 0
+	depth = 0
+	for current in instructions:
+		command = current.split(' ')[0]
+		unit = int(current.split(' ')[1])
+		if command == 'forward':
+			position += unit
+		elif command == 'down':
+			depth += unit
+		elif command == 'up':
+			depth -= unit
+
+	return position * depth
 
 def level2(input):
-	return "Not implemented"
+	instructions = input.strip().split('\n')
+	position = 0
+	depth = 0
+	aim = 0
+	for current in instructions:
+		command = current.split(' ')[0]
+		unit = int(current.split(' ')[1])
+		if command == 'forward':
+			position += unit
+			depth += aim * unit
+		elif command == 'down':
+			aim += unit
+		elif command == 'up':
+			aim -= unit
+
+	return position * depth
