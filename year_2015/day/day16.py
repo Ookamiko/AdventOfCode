cfd584f8812912691d2bc4bccb5d7e1d9a38f2fc
Valentin Dewilde <valentind@dufour.be>
2021-12-02 08:51:03 +0100

    [ADD] Resolve stage 2015-16-1 and 2015-16-2

--- year_2015/day/day16.py
+++ year_2015/day/day16.py
@@ -1,7 +1,69 @@
 #!/usr/bin/python
 
+import re as regex
+
+ticket =  {
+	'children': 3,
+	'cats': 7,
+	'samoyeds': 2,
+	'pomeranians': 3,
+	'akitas': 0,
+	'vizslas': 0,
+	'goldfish': 5,
+	'trees': 3,
+	'cars': 2,
+	'perfumes': 1
+}
+
 def level1(input):
-	return "Not implemented"
+	reg = r'^Sue (\d+):(.*)$'
+	smallReg = r'([a-z]+): (\d+)'
+	instructions = input.strip().split('\n')
+	toReturn = ""
+
+	for instruction in instructions:
+		matches = regex.match(reg, instruction)
+		if matches:
+			groups = matches.groups()
+			correct = True
+			for current in groups[1].strip().split(', '):
+				attribute = current.split(':')[0].strip()
+				value = int(current.split(':')[1].strip())
+				if not(ticket[attribute] == value):
+					correct = False
+					break
+			if correct:
+				toReturn = groups[0]
+				break
+
+	return toReturn
 
 def level2(input):
+	reg = r'^Sue (\d+):(.*)$'
+	smallReg = r'([a-z]+): (\d+)'
+	instructions = input.strip().split('\n')
+	toReturn = ""
+
+	for instruction in instructions:
+		matches = regex.match(reg, instruction)
+		if matches:
+			groups = matches.groups()
+			correct = True
+			for current in groups[1].strip().split(', '):
+				attribute = current.split(':')[0].strip()
+				value = int(current.split(':')[1].strip())
+				if (attribute == 'cats' or attribute == 'trees') and ticket[attribute] >= value:
+					correct = False
+					break
+				elif (attribute == 'pomeranians' or attribute == 'goldfish') and ticket[attribute] <= value:
+					correct = False
+					break
+				elif attribute != 'pomeranians' and attribute != 'goldfish' and attribute != 'cats' and attribute != 'trees' and ticket[attribute] != value:
+					correct = False
+					break
+			if correct:
+				toReturn = groups[0]
+				break
+
+	return toReturn
 	return "Not implemented"
