13c3ec4718ed5b6ee3c6af2bfb2bd93f9c9063b4
Valentin Dewilde <valentind@dufour.be>
2021-12-02 08:49:59 +0100

    [ADD] Resolve stage 2021-1-1 and 2021-1-2

--- year_2021/day/day01.py
+++ year_2021/day/day01.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
 
 def level1(input):
-	return "Not implemented"
+	count_inc = 0
+	depth = input.strip().split('\n')
+	for i in range(len(depth) - 1):
+		if (int(depth[i]) < int(depth[i + 1])):
+			count_inc += 1
+
+	return count_inc
 
 def level2(input):
-	return "Not implemented"
+	count_inc = 0
+	depth = input.strip().split('\n')
+	for i in range(len(depth) - 3):
+		first_slide = int(depth[i]) + int(depth[i + 1]) + int(depth[i + 2])
+		second_slide = int(depth[i + 1]) + int(depth[i + 2]) + int(depth[i + 3])
+		if first_slide < second_slide:
+			count_inc += 1
+
+	return count_inc
