15609ca79ce828df23da3fed09d4ae46885c0851
Valentin Dewilde <valentind@dufour.be>
2021-12-06 10:40:13 +0100

    [ADD] Resolve stage 2021-3-1 and 2021-3-2

--- year_2021/day/day03.py
+++ year_2021/day/day03.py
@@ -1,7 +1,58 @@
 #!/usr/bin/python
 
+def GetGammaAndEpsylonRate(instructions):
+	gamma = []
+	epsylon = []
+	for j in range(len(instructions[0])):
+		bits = []
+		for i in range(len(instructions)):
+			bits.append(instructions[i][j])
+		if bits.count('1') >= len(instructions) / 2:
+			gamma.append('1')
+			epsylon.append('0')
+		else:
+			gamma.append('0')
+			epsylon.append('1')
+	return [''.join(gamma), ''.join(epsylon)]
+
+def GetOxygenRating(instructions, indice):
+	if len(instructions) == 1:
+		return instructions[0]
+	else:
+		aone = []
+		azero = []
+		for current in instructions:
+			if current[indice] == '1':
+				aone.append(current)
+			else:
+				azero.append(current)
+		if len(aone) >= len(azero):
+			return GetOxygenRating(aone, indice + 1)
+		else:
+			return GetOxygenRating(azero, indice + 1)
+
+def GetCO2Rating(instructions, indice):
+	if len(instructions) == 1:
+		return instructions[0]
+	else:
+		aone = []
+		azero = []
+		for current in instructions:
+			if current[indice] == '1':
+				aone.append(current)
+			else:
+				azero.append(current)
+		if len(aone) >= len(azero):
+			return GetCO2Rating(azero, indice + 1)
+		else:
+			return GetCO2Rating(aone, indice + 1)
+
+
 def level1(input):
-	return "Not implemented"
+	result = GetGammaAndEpsylonRate(input.strip().split('\n'))
+	return int(result[0], 2) * int(result[1], 2)
 
 def level2(input):
-	return "Not implemented"
+	oxygen = GetOxygenRating(input.strip().split('\n'), 0)
+	co2 = GetCO2Rating(input.strip().split('\n'), 0)
+	return int(oxygen, 2) * int(co2, 2)
