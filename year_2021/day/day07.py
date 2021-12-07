542cf71a250b766afd1898405765a3f4d3e05ea3
Valentin Dewilde <valentind@dufour.be>
2021-12-07 10:52:22 +0100

    [ADD] Resolve stage 2021-7-1 and 2021-7-2

--- year_2021/day/day07.py
+++ year_2021/day/day07.py
@@ -1,7 +1,43 @@
 #!/usr/bin/python
 
+import sys
+
+def CalculConso(positions, place):
+	conso = 0
+	for position in positions:
+		conso += abs(position - place)
+	return conso
+
+def CalculRealConso(positions, place):
+	conso = 0
+	for position in positions:
+		distance = abs(position - place)
+		conso += distance * (distance + 1) // 2
+	return conso	
+
 def level1(input):
-	return "Not implemented"
+	position = [int(value) for value in input.strip().split(',')]
+	position.sort()
+	minConso = sys.maxsize
+	for i in range(position[0], position[len(position) - 1] + 1):
+		conso = CalculConso(position, i)
+		if conso <= minConso:
+			minConso = conso
+		else:
+			break
+
+	return minConso
 
 def level2(input):
-	return "Not implemented"
+	position = [int(value) for value in input.strip().split(',')]
+	position.sort()
+	minConso = sys.maxsize
+	for i in range(position[0], position[len(position) - 1] + 1):
+		conso = CalculRealConso(position, i)
+		print(conso)
+		if conso <= minConso:
+			minConso = conso
+		else:
+			break
+
+	return minConso
