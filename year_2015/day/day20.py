1152b789f45ce87e1cd553fddd97a653aa281d27
Valentin Dewilde <valentind@dufour.be>
2021-12-10 10:23:04 +0100

    [ADD] Resolve stage 2015-20-1 and 2015-20-2

--- year_2015/day/day20.py
+++ year_2015/day/day20.py
@@ -1,7 +1,42 @@
 #!/usr/bin/python
 
+import math
+
+def GetMultipleForHouse(house):
+	result = []
+	for i in range(1, int(math.sqrt(house)) + 1):
+		if house % i == 0:
+			if house / i == i:
+				result.append(i)
+			else:
+				result.append(i)
+				result.append(int(house / i))
+	return result
+
+def GetMultipleForHouseNotInfinite(house):
+	result = []
+	for i in range(1, min(house, 50) + 1):
+		if house % i == 0:
+			result.append(int(house / i))
+	return result
+
 def level1(input):
-	return "Not implemented"
+	house = 0
+	present = 0
+	while present < int(input.strip()):
+		house += 1
+		multiples = GetMultipleForHouse(house)
+		present = sum([value * 10 for value in multiples])
+		print(str(house) + ": " + str(present))
+
+	return house
 
 def level2(input):
-	return "Not implemented"
+	house = 0
+	present = 0
+	while present < int(input.strip()):
+		house += 1
+		multiples = GetMultipleForHouseNotInfinite(house)
+		present = sum([value * 11 for value in multiples])
+		print(str(house) + ": " + str(present))
+	return house
