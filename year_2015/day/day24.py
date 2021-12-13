5763e98f8ec9c78b36ad8ebb7e050fd3e5273246
Valentin Dewilde <valentind@dufour.be>
2021-12-13 20:56:08 +0100

    [ADD] Resolve stage 2015-24-1 and 2015-24-2

--- year_2015/day/day24.py
+++ year_2015/day/day24.py
@@ -1,7 +1,49 @@
 #!/usr/bin/python
 
+import sys
+
+def GetAllPossibilitiesForSize(packages, size):
+	result = []
+	for i in range(len(packages)):
+		weight = packages[i]
+		if weight > size:
+			continue
+		elif weight == size:
+			result.append([weight])
+		else:
+			tmp = GetAllPossibilitiesForSize(packages[i + 1:], size - weight)
+			for current in tmp:
+				result.append([weight] + current)
+	return result
+
+def Multiply(array):
+	result = 1
+	for number in array:
+		result *= number
+	return result
+
 def level1(input):
-	return "Not implemented"
+	packages = [int(value) for value in input.strip().split('\n')]
+	group_size = sum(packages) // 3
+	allPossibilities = GetAllPossibilitiesForSize(packages, group_size)
+	allPossibilities.sort(key=lambda x: len(x))
+	best_size = len(allPossibilities[0])
+	best_entanglement = sys.maxsize
+	i = 0
+	while len(allPossibilities[i]) == best_size:
+		best_entanglement = min(best_entanglement, Multiply(allPossibilities[i]))
+		i += 1
+	return best_entanglement
 
 def level2(input):
-	return "Not implemented"
+	packages = [int(value) for value in input.strip().split('\n')]
+	group_size = sum(packages) // 4
+	allPossibilities = GetAllPossibilitiesForSize(packages, group_size)
+	allPossibilities.sort(key=lambda x: len(x))
+	best_size = len(allPossibilities[0])
+	best_entanglement = sys.maxsize
+	i = 0
+	while len(allPossibilities[i]) == best_size:
+		best_entanglement = min(best_entanglement, Multiply(allPossibilities[i]))
+		i += 1
+	return best_entanglement
