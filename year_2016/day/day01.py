7fe461b4e8062188fc4e5e540ac5cfdebe364137
Valentin Dewilde <valentind@dufour.be>
2021-12-13 20:59:34 +0100

    [ADD] Resolve stage 2016-1-1 and 2016-1-2

--- year_2016/day/day01.py
+++ year_2016/day/day01.py
@@ -1,7 +1,55 @@
 #!/usr/bin/python
 
+import re as regex
+
+def AlreadySeen(positionSeen, position):
+	for x,y in positionSeen:
+		if x == position[0] and y == position[1]:
+			return True
+	return False
+
 def level1(input):
-	return "Not implemented"
+	compass = [{'x':0, 'y':1}, {'x':-1, 'y':0}, {'x':0, 'y':-1}, {'x':1, 'y':0}]
+	posCompass = 0
+	x = 0
+	y = 0
+	reg = r'(R|L)(\d+)'
+	for current in input.strip().split(', '):
+		matches = regex.match(reg, current)
+		if matches:
+			groups = matches.groups()
+			if groups[0] == 'R':
+				posCompass = (posCompass + 1) % 4
+			else:
+				posCompass = (posCompass + 4 - 1) % 4
+
+			x += compass[posCompass]['x'] * int(groups[1])
+			y += compass[posCompass]['y'] * int(groups[1])
+
+	return abs(x) + abs(y)
 
 def level2(input):
-	return "Not implemented"
+	compass = [{'x':0, 'y':1}, {'x':-1, 'y':0}, {'x':0, 'y':-1}, {'x':1, 'y':0}]
+	posCompass = 0
+	x = 0
+	y = 0
+	positionSeen = [[x,y]]
+	reg = r'(R|L)(\d+)'
+	for current in input.strip().split(', '):
+		matches = regex.match(reg, current)
+		if matches:
+			groups = matches.groups()
+			if groups[0] == 'R':
+				posCompass = (posCompass + 1) % 4
+			else:
+				posCompass = (posCompass + 4 - 1) % 4
+
+			for i in range(int(groups[1])):
+				x += compass[posCompass]['x']
+				y += compass[posCompass]['y']
+				if AlreadySeen(positionSeen, [x,y]):
+					return abs(x) + abs(y)
+				else:
+					positionSeen.append([x,y])
+
+	return "Not Found"
