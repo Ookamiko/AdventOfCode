c496b76ed49cd9f75d8691b88b4aee0079e77060
Valentin Dewilde <valentind@dufour.be>
2021-12-14 16:56:35 +0100

    [ADD] Resolve stage 2016-6-1 and 2016-6-2

--- year_2016/day/day06.py
+++ year_2016/day/day06.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
 
+def RetrieveCode(instructions, reverseJam=True):
+	code = ''
+	length = len(instructions.strip().split('\n')[0]) + 1
+	for i in range(length - 1):
+		tmp = [instructions[j] for j in range(len(instructions)) if j % length == i]
+		tmpLetter = list(dict.fromkeys(tmp))
+		tmpOrder = []
+		for letter in tmpLetter:
+			tmpOrder.append([letter, tmp.count(letter)])
+		tmpOrder.sort(key=lambda x: x[1], reverse=reverseJam)
+		code += tmpOrder[0][0]
+
+	return code
+
 def level1(input):
-	return "Not implemented"
+	return RetrieveCode(input)
 
 def level2(input):
-	return "Not implemented"
+	return RetrieveCode(input, False)
