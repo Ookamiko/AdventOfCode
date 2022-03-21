d1e7a09a7758e0648f56789882f0d20f4a1be090
Valentin Dewilde <valentind@dufour.be>
2022-03-21 16:15:10 +0100

    [ADD] Resolve stage 2018-1-1 and 2018-1-2

--- year_2018/day/day01.py
+++ year_2018/day/day01.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python
 
 def level1(input):
-	return "Not implemented"
+	lines = input.strip().split('\n')
+	result = 0
+	for line in lines:
+		result += int(line.replace('+', ''))
+	return result
 
 def level2(input):
-	return "Not implemented"
+	lines = input.strip().split('\n')
+	result = 0
+	save = [0]
+	finish = False
+
+	while not(finish):
+		for line in lines:
+			result += int(line.replace('+', ''))
+			if (result in save):
+				finish = True
+				break
+			else:
+				save.append(result)
+
+	return result
