ec31e7ac26af4b6a1f1213dc45aea5c3f85da665
Valentin Dewilde <valentind@dufour.be>
2022-12-02 09:31:28 +0100

    [ADD] Resolve stage 2022-2-1 and 2022-2-2

--- year_2022/day/day02.py
+++ year_2022/day/day02.py
@@ -1,7 +1,69 @@
 #!/usr/bin/python
 
 def level1(input):
-	return "Not implemented"
+	instructions = input.strip().split('\n')
+	score = 0
+
+	for line in instructions:
+		chars = line.split(' ')
+
+		if chars[1] == 'X':
+			score += 1
+			if chars[0] == 'A':
+				score += 3
+			elif chars[0] == 'B':
+				score += 0
+			else:
+				score += 6
+		elif chars[1] == 'Y':
+			score += 2
+			if chars[0] == 'A':
+				score += 6
+			elif chars[0] == 'B':
+				score += 3
+			else:
+				score += 0
+		else:
+			score += 3
+			if chars[0] == 'A':
+				score += 0
+			elif chars[0] == 'B':
+				score += 6
+			else:
+				score += 3
+
+	return score
 
 def level2(input):
-	return "Not implemented"
+	instructions = input.strip().split('\n')
+	score = 0
+
+	for line in instructions:
+		chars = line.split(' ')
+
+		if chars[1] == 'X':
+			score += 0
+			if chars[0] == 'A':
+				score += 3
+			elif chars[0] == 'B':
+				score += 1
+			else:
+				score += 2
+		elif chars[1] == 'Y':
+			score += 3
+			if chars[0] == 'A':
+				score += 1
+			elif chars[0] == 'B':
+				score += 2
+			else:
+				score += 3
+		else:
+			score += 6
+			if chars[0] == 'A':
+				score += 2
+			elif chars[0] == 'B':
+				score += 3
+			else:
+				score += 1
+
+	return score
