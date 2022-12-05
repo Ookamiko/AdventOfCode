4c37d6b3f1774bf9911ec0bff5a8fb50f0f62fe2
Valentin Dewilde <valentind@dufour.be>
2022-12-05 15:02:43 +0100

    [ADD] Resolve stage 2022-4-1 and 2022-4-2

--- year_2022/day/day04.py
+++ year_2022/day/day04.py
@@ -1,7 +1,31 @@
 #!/usr/bin/python
 
+import re as regex
+
 def level1(input):
-	return "Not implemented"
+	pairs = input.strip().split('\n')
+	result = 0
+
+	for pair in pairs:
+		splits = [int(x) for x in regex.findall(r'\d+', pair)]
+
+		if (splits[0] <= splits[2] and splits[1] >= splits[3]) or (splits[2] <= splits[0] and splits[3] >= splits[1]):
+			result += 1
+
+	return result
 
 def level2(input):
-	return "Not implemented"
+	pairs = input.strip().split('\n')
+	result = 0
+
+	for pair in pairs:
+		splits = [int(x) for x in regex.findall(r'\d+', pair)]
+
+		if ((splits[0] >= splits[2] and splits[0] <= splits[3]) or
+			(splits[1] >= splits[2] and splits[1] <= splits[3]) or
+			(splits[2] >= splits[0] and splits[2] <= splits[1]) or
+			(splits[3] >= splits[0] and splits[3] <= splits[1])
+			):
+			result += 1
+
+	return result
