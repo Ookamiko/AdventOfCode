7986fe431d6cd9bafb20cdf64975aeb8cf2feffc
Valentin Dewilde <valentind@dufour.be>
2022-12-06 08:50:31 +0100

    [ADD] Resolve stage 2022-6-1 and 2022-6-2

--- year_2022/day/day06.py
+++ year_2022/day/day06.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python
 
 def level1(input):
-	return "Not implemented"
+	line = input.strip()
+	result = 4
+
+	for i in range(len(line)):
+		letters = [x for x in line[i:i+4]]
+		test = [*set(letters)]
+		if len(test) == 4:
+			break
+		else:
+			result += 1
+
+	return result
 
 def level2(input):
-	return "Not implemented"
+	line = input.strip()
+	result = 14
+
+	for i in range(len(line)):
+		letters = [x for x in line[i:i+14]]
+		test = [*set(letters)]
+		if len(test) == 14:
+			break
+		else:
+			result += 1
+
+	return result
