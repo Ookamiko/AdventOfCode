4b1e4ea640c0c5054c14c3c0c307255eaf03404f
Valentin Dewilde <valentind@dufour.be>
2022-12-12 16:40:11 +0100

    [ADD] Resolve stage 2016-19-1 and 2016-19-2

--- year_2016/day/day19.py
+++ year_2016/day/day19.py
@@ -1,7 +1,35 @@
 #!/usr/bin/python
 
 def level1(input):
-	return "Not implemented"
+	participants = int(input.strip())
+	participants = 13
+	indice = 1
+	cycle = 1
+	while participants > 1:
+		if participants % 2 != 0:
+			indice += 2 ** cycle
+		cycle += 1
+		participants = participants // 2
+
+	return indice
+
+
+## Solution from https://oeis.org/A334473 (Josephus problem)
+def highest_power_of_3(n):
+    option = 0
+    while 3**option <= n:
+        option +=1
+    return 3 ** (option -1)
+
+def answer(n):
+    x = highest_power_of_3(n)
+    if x == n:
+        return x
+    else:
+        if n < 2 * x:
+            return n%x
+        else:
+            return x + 2 * (n%x)
 
 def level2(input):
-	return "Not implemented"
+	return answer(int(input.strip()))
