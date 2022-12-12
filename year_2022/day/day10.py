3df70c7b0d520644df5a0c2b67b26c38d3b8bb78
Valentin Dewilde <valentind@dufour.be>
2022-12-12 16:40:34 +0100

    [ADD] Resolve stage 2022-10-1 and 2022-10-2

--- year_2022/day/day10.py
+++ year_2022/day/day10.py
@@ -1,7 +1,66 @@
 #!/usr/bin/python
 
 def level1(input):
-	return "Not implemented"
+	#input = 'addx 15\naddx -11\naddx 6\naddx -3\naddx 5\naddx -1\naddx -8\naddx 13\naddx 4\nnoop\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx -35\naddx 1\naddx 24\naddx -19\naddx 1\naddx 16\naddx -11\nnoop\nnoop\naddx 21\naddx -15\nnoop\nnoop\naddx -3\naddx 9\naddx 1\naddx -3\naddx 8\naddx 1\naddx 5\nnoop\nnoop\nnoop\nnoop\nnoop\naddx -36\nnoop\naddx 1\naddx 7\nnoop\nnoop\nnoop\naddx 2\naddx 6\nnoop\nnoop\nnoop\nnoop\nnoop\naddx 1\nnoop\nnoop\naddx 7\naddx 1\nnoop\naddx -13\naddx 13\naddx 7\nnoop\naddx 1\naddx -33\nnoop\nnoop\nnoop\naddx 2\nnoop\nnoop\nnoop\naddx 8\nnoop\naddx -1\naddx 2\naddx 1\nnoop\naddx 17\naddx -9\naddx 1\naddx 1\naddx -3\naddx 11\nnoop\nnoop\naddx 1\nnoop\naddx 1\nnoop\nnoop\naddx -13\naddx -19\naddx 1\naddx 3\naddx 26\naddx -30\naddx 12\naddx -1\naddx 3\naddx 1\nnoop\nnoop\nnoop\naddx -9\naddx 18\naddx 1\naddx 2\nnoop\nnoop\naddx 9\nnoop\nnoop\nnoop\naddx -1\naddx 2\naddx -37\naddx 1\naddx 3\nnoop\naddx 15\naddx -21\naddx 22\naddx -6\naddx 1\nnoop\naddx 2\naddx 1\nnoop\naddx -10\nnoop\nnoop\naddx 20\naddx 1\naddx 2\naddx 2\naddx -6\naddx -11\nnoop\nnoop\nnoop\n'
+	instructions = input.strip().split('\n')
+	buffer = 0
+	breaker = [x for x in range(20, 221, 40)]
+	count = 0
+	reg = 1
+	cycle = 0
+	cursor = 0
+
+	while cursor < len(instructions):
+		cycle += 1
+
+		if cycle in breaker:
+			count += cycle * reg
+			if cycle == breaker[-1]:
+				break
+
+		if buffer != 0:
+			reg += buffer
+			buffer = 0
+		else:
+			cmd = instructions[cursor].split(' ')
+			if len(cmd) == 2:
+				buffer = int(cmd[1])
+			cursor += 1
+
+	return count
 
 def level2(input):
-	return "Not implemented"
+	#input = 'addx 15\naddx -11\naddx 6\naddx -3\naddx 5\naddx -1\naddx -8\naddx 13\naddx 4\nnoop\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx 5\naddx -1\naddx -35\naddx 1\naddx 24\naddx -19\naddx 1\naddx 16\naddx -11\nnoop\nnoop\naddx 21\naddx -15\nnoop\nnoop\naddx -3\naddx 9\naddx 1\naddx -3\naddx 8\naddx 1\naddx 5\nnoop\nnoop\nnoop\nnoop\nnoop\naddx -36\nnoop\naddx 1\naddx 7\nnoop\nnoop\nnoop\naddx 2\naddx 6\nnoop\nnoop\nnoop\nnoop\nnoop\naddx 1\nnoop\nnoop\naddx 7\naddx 1\nnoop\naddx -13\naddx 13\naddx 7\nnoop\naddx 1\naddx -33\nnoop\nnoop\nnoop\naddx 2\nnoop\nnoop\nnoop\naddx 8\nnoop\naddx -1\naddx 2\naddx 1\nnoop\naddx 17\naddx -9\naddx 1\naddx 1\naddx -3\naddx 11\nnoop\nnoop\naddx 1\nnoop\naddx 1\nnoop\nnoop\naddx -13\naddx -19\naddx 1\naddx 3\naddx 26\naddx -30\naddx 12\naddx -1\naddx 3\naddx 1\nnoop\nnoop\nnoop\naddx -9\naddx 18\naddx 1\naddx 2\nnoop\nnoop\naddx 9\nnoop\nnoop\nnoop\naddx -1\naddx 2\naddx -37\naddx 1\naddx 3\nnoop\naddx 15\naddx -21\naddx 22\naddx -6\naddx 1\nnoop\naddx 2\naddx 1\nnoop\naddx -10\nnoop\nnoop\naddx 20\naddx 1\naddx 2\naddx 2\naddx -6\naddx -11\nnoop\nnoop\nnoop\n'
+	instructions = input.strip().split('\n')
+	buffer = 0
+	breaker = [x for x in range(40, 241, 40)]
+	screen = '\n'
+	reg = 1
+	cycle = 0
+	cursor = 0
+
+	while cursor < len(instructions):
+
+		if cycle >= reg - 1 and cycle <= reg + 1:
+			screen += "#"
+		else:
+			screen += " "
+
+		cycle += 1
+
+		if cycle in breaker:
+			screen += "\n"
+			cycle = 0
+			if cycle == breaker[-1]:
+				break
+
+		if buffer != 0:
+			reg += buffer
+			buffer = 0
+		else:
+			cmd = instructions[cursor].split(' ')
+			if len(cmd) == 2:
+				buffer = int(cmd[1])
+			cursor += 1
+
+	return screen
