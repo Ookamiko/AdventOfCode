fd703f34c95d65ce1829285bf52e31e6c56679be
Valentin Dewilde <valentind@dufour.be>
2022-12-14 16:49:33 +0100

    [ADD] Resolve stage 2017-2-1 and 2017-2-2

--- year_2017/day/day02.py
+++ year_2017/day/day02.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python
 
 def level1(input):
-	return "Not implemented"
+	lines = input.strip().split('\n')
+	count = 0
+	for line in lines:
+		numbers = [int(x) for x in line.split('\t')]
+		numbers.sort()
+		count += numbers[len(numbers) - 1] - numbers[0]
+
+	return count
 
 def level2(input):
-	return "Not implemented"
+	lines = input.strip().split('\n')
+	count = 0
+	for line in lines:
+		numbers = [int(x) for x in line.split('\t')]
+		numbers.sort(reverse=True)
+		for i in range(len(numbers) - 1):
+			for j in range(i + 1, len(numbers)):
+				if numbers[i] % numbers[j] == 0:
+					count += numbers[i] // numbers[j]
+
+	return count
