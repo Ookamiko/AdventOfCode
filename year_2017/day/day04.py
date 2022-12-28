525c5a38c9733bb58363918c544a115f1f3c047c
Valentin Dewilde <valentind@dufour.be>
2022-12-28 10:02:33 +0100

    [ADD] Resolve stage 2017-4-1 and 2017-4-2

--- year_2017/day/day04.py
+++ year_2017/day/day04.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python
 
 def level1(input):
-	return "Not implemented"
+	result = 0
+	for passphrase in input.strip().split('\n'):
+		words = passphrase.split(' ')
+		dict_words = [*set(words)]
+		if len(words) == len(dict_words):
+			result += 1
+	return result
 
 def level2(input):
-	return "Not implemented"
+	result = 0
+	for passphrase in input.strip().split('\n'):
+		words = []
+		for word in passphrase.split(' '):
+			chars = [x for x in word]
+			chars.sort()
+			words.append(''.join(chars))
+
+		dict_words = [*set(words)]
+		if len(words) == len(dict_words):
+			result += 1
+
+	return result
