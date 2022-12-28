6445d8f8141c0688a9d94d53f18484b561eb3424
Valentin Dewilde <valentind@dufour.be>
2022-12-28 10:02:20 +0100

    [ADD] Resolve stage 2017-3-1 and 2017-3-2

--- year_2017/day/day03.py
+++ year_2017/day/day03.py
@@ -1,7 +1,125 @@
 #!/usr/bin/python
 
 def level1(input):
-	return "Not implemented"
+	num = int(input)
+	power = 1
+	while power ** 2 < num:
+		power += 2
+
+	rest = power ** 2 - num
+
+	while rest > power:
+		rest -= power - 1
+
+	if rest > (power - 1) // 2:
+		middle = (power - 1) // 2
+		rest = middle - (rest - middle)
+
+	return (power - 1) // 2 + (((power - 1) // 2) - rest)
+
+def get_next_value(x, y, mapping_ord, mapping_val):
+	neightboor = [
+		str(x-1) + ',' + str(y-1),
+		str(x) + ',' + str(y-1),
+		str(x+1) + ',' + str(y-1),
+		str(x-1) + ',' + str(y),
+		str(x+1) + ',' + str(y),
+		str(x-1) + ',' + str(y+1),
+		str(x) + ',' + str(y+1),
+		str(x+1) + ',' + str(y+1),
+	]
+
+	result = 0
+
+	for coord in neightboor:
+		if not(coord in mapping_ord):
+			continue
+		index = mapping_ord.index(coord)
+		result += mapping_val[index]
+
+	return result
 
 def level2(input):
-	return "Not implemented"
+	maximum = int(input)
+	mapping_ord = ['0,0']
+	mapping_val = [1]
+
+	count = 3
+	x = 0
+	y = 0
+
+	finish = False
+
+	result = 0
+
+	while not(finish):
+		x += 1
+		value = get_next_value(x, y, mapping_ord, mapping_val)
+
+		if value > maximum:
+			result = value
+			break
+
+		mapping_ord.append(str(x) + ',' + str(y))
+		mapping_val.append(value)
+
+		for i in range(count - 2):
+			y -= 1
+			value = get_next_value(x, y, mapping_ord, mapping_val)
+
+			if value > maximum:
+				result = value
+				finish = True
+				break
+
+			mapping_ord.append(str(x) + ',' + str(y))
+			mapping_val.append(value)
+
+		if finish:
+			break
+
+		for i in range(count - 1):
+			x -= 1
+			value = get_next_value(x, y, mapping_ord, mapping_val)
+
+			if value > maximum:
+				result = value
+				finish = True
+				break
+
+			mapping_ord.append(str(x) + ',' + str(y))
+			mapping_val.append(value)
+
+		if finish:
+			break
+
+		for i in range(count - 1):
+			y += 1
+			value = get_next_value(x, y, mapping_ord, mapping_val)
+
+			if value > maximum:
+				result = value
+				finish = True
+				break
+
+			mapping_ord.append(str(x) + ',' + str(y))
+			mapping_val.append(value)
+
+		if finish:
+			break
+
+		for i in range(count - 1):
+			x += 1
+			value = get_next_value(x, y, mapping_ord, mapping_val)
+
+			if value > maximum:
+				result = value
+				finish = True
+				break
+
+			mapping_ord.append(str(x) + ',' + str(y))
+			mapping_val.append(value)
+
+		count += 2
+
+	return result
