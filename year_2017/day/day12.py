4f0cfe9328354ca71b7bf7a4a96e6fa2beb17f81
Valentin Dewilde <valentind@dufour.be>
2023-12-06 12:09:56 +0100

    [ADD] Resolve stage 2017-12-1 and 2017-12-2

--- year_2017/day/day12.py
+++ year_2017/day/day12.py
@@ -1,7 +1,44 @@
 #!/usr/bin/python
 
+import re as regex
+
+def define_group(links, index, group=[]):
+	group += [index]
+
+	for new_index in links[index]:
+		if new_index in group:
+			continue
+
+		define_group(links, new_index, group)
+
+	return group
+
 def level1(input):
-	return "Not implemented"
+	# Test value
+	# input = '0 <-> 2\n1 <-> 1\n2 <-> 0, 3, 4\n3 <-> 2, 4\n4 <-> 2, 3, 6\n5 <-> 6\n6 <-> 4, 5\n'
+	tmp = [x.split('<->')[1] for x in input.strip().split('\n')]
+	links = []
+	for current in tmp:
+		links.append([int(x) for x in regex.findall(r'\d+', current)])
+
+	return len(define_group(links, 0))
 
 def level2(input):
-	return "Not implemented"
+	# Test value
+	# input = '0 <-> 2\n1 <-> 1\n2 <-> 0, 3, 4\n3 <-> 2, 4\n4 <-> 2, 3, 6\n5 <-> 6\n6 <-> 4, 5\n'
+	tmp = [x.split('<->')[1] for x in input.strip().split('\n')]
+	links = []
+	for current in tmp:
+		links.append([int(x) for x in regex.findall(r'\d+', current)])
+
+	found = []
+	nbr_group = 0
+
+	for i in range(len(links)):
+		if i in found:
+			continue
+
+		found += define_group(links, i)
+		nbr_group += 1
+
+	return nbr_group
