1accd4b534a598f151a3ee39ef85c9426c8d5e74
Valentin Dewilde <valentind@dufour.be>
2023-12-06 15:09:33 +0100

    [ADD] Resolve stage 2017-13-1 and 2017-13-2

--- year_2017/day/day13.py
+++ year_2017/day/day13.py
@@ -1,7 +1,45 @@
 #!/usr/bin/python
 
+import re as regex
+
+def is_caught(time, rang):
+	cycle = (rang - 1) * 2
+	return cycle > 0 and (time % cycle) == 0
+
 def level1(input):
-	return "Not implemented"
+	# Test value
+	# input = '0: 3\n1: 2\n4: 4\n6: 4\n'
+	tmp = input.strip().split('\n')
+	firewall = []
+	for current in tmp:
+		firewall.append([int(x) for x in regex.findall(r'\d+', current)])
+
+	start = 0
+	result = 0
+
+	for layer in firewall:
+		if is_caught(start + layer[0], layer[1]):
+			result += layer[0] * layer[1]
+
+	return result
 
 def level2(input):
-	return "Not implemented"
+	# Test value
+	# input = '0: 3\n1: 2\n4: 4\n6: 4\n'
+	tmp = input.strip().split('\n')
+	firewall = []
+	for current in tmp:
+		firewall.append([int(x) for x in regex.findall(r'\d+', current)])
+
+	start = 0
+	safe = False
+
+	while not(safe):
+		start += 1
+		safe = True
+		for layer in firewall:
+			if is_caught(start + layer[0], layer[1]):
+				safe = False
+				break
+
+	return start
