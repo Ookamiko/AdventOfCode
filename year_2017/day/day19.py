e421165efff4578642e0f89d6be6eff33281ea0f
Valentin Dewilde <valentind@dufour.be>
2023-12-08 10:04:56 +0100

    [ADD] Resolve stage 2017-19-1 and 2017-19-2

--- year_2017/day/day19.py
+++ year_2017/day/day19.py
@@ -1,7 +1,66 @@
 #!/usr/bin/python
 
 def level1(input):
-	return "Not implemented"
+	# Test value
+	# input = '     |          \n     |  +--+    \n     A  |  C    \n F---|--|-E---+ \n     |  |  |  D \n     +B-+  +--+ \n'
+	lines = input.split('\n')
+	nbr_char = len(lines[0])
+	index = lines[0].index('|')
+	direction = nbr_char
+	maze = ''.join(lines[:len(lines) - 1])
+	path = []
+
+	result = ''
+
+	while maze[index] != ' ':
+		char = maze[index]
+		path.append(char)
+
+		if char == '+':
+			if direction == 1 or direction == -1:
+				if index - nbr_char > 0 and maze[index - nbr_char] != ' ':
+					direction = nbr_char * -1
+				else:
+					direction = nbr_char
+			else:
+				if index % nbr_char != 0 and maze[index - 1] != ' ':
+					direction = -1
+				else:
+					direction = 1
+
+		elif char != '|' and char != '-':
+			result += char
+
+		index += direction
+
+	return result
 
 def level2(input):
-	return "Not implemented"
+	# Test value
+	# input = '     |          \n     |  +--+    \n     A  |  C    \n F---|--|-E---+ \n     |  |  |  D \n     +B-+  +--+ \n'
+	lines = input.split('\n')
+	nbr_char = len(lines[0])
+	index = lines[0].index('|')
+	direction = nbr_char
+	maze = ''.join(lines[:len(lines) - 1])
+	path = []
+
+	while maze[index] != ' ':
+		char = maze[index]
+		path.append(char)
+
+		if char == '+':
+			if direction == 1 or direction == -1:
+				if index - nbr_char > 0 and maze[index - nbr_char] != ' ':
+					direction = nbr_char * -1
+				else:
+					direction = nbr_char
+			else:
+				if index % nbr_char != 0 and maze[index - 1] != ' ':
+					direction = -1
+				else:
+					direction = 1
+
+		index += direction
+
+	return len(path)
