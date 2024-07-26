e1edeb40a93ab475ca6720eb6c652f4fda4d9ef9
Valentin 'Ookamiko' Dewilde <valentin.dewilde@hotmail.be>
2024-07-26 12:00:59 +0200

    [INIT] Year folder and code

--- adventofcode.py
+++ adventofcode.py
@@ -6,6 +6,7 @@ import sys
 import requests
 
 from template.generic import get_solve_function as fct_template
+from year_2022.generic import get_solve_function as fct_2022
 
 # Session ID. Check README file to see how to retrieve this value.
 CONST_ID = ""
@@ -41,5 +42,8 @@ Looked at the README file to know how to set it up.
         """)
 
     fct = default
+
+    if year == '2022':
+        fct = fct_2022(day,level)
 
     return fct(input)
