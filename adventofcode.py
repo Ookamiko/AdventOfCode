39dd4c53135ed9396d450e72301bda0d55316a56
Valentin 'Ookamiko' Dewilde <valentin.dewilde@hotmail.be>
2024-07-26 08:59:18 +0200

    [INIT] Initial state
    + Main method
    + Function to retrieve input from adventofcode website
    + template folder

---
+++ adventofcode.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+import requests
+
+from datetime import datetime, timedelta
+from template.generic import get_solve_function as fct_template
+
+# Session ID. Check README file to see how to retrieve this value.
+CONST_ID = ""
+CONST_URL = f"https://adventofcode.com/"
+
+def default(input):
+    sys.exit("""
+No function define for year passed as argument.
+Looked at the README file to know how to define a new year.
+        """
+        )
+
+def get_input(year, day):
+    cookies = {
+        'session': CONST_ID,
+    }
+
+    res = requests.get(CONST_URL + year + "/day/" + day + "/input", cookies=cookies)
+
+    if res.status_code == 404:
+        sys.exit('No input found. Are you sure to ask for a year-day available ?')
+    else:
+        sys.exit(res.text)
+
+    return res.content.decode('UTF-8')
+
+def solve(year, day, level):
+
+    if CONST_ID == "":
+        sys.exit("""
+No session ID defined.
+Looked at the README file to know how to set it up.
+        """)
+
+    input = get_input(year, day)
+    fct = default
+
+    return fct(input)
---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import sys
+import datetime
+
+import adventofcode
+
+try:
+	if int(sys.argv[1]) < 2015:
+		sys.exit("Year defined must be greater or equals to 2015.")
+
+	if int(sys.argv[2]) not in range(1,26):
+		sys.exit("Day defined must be a value between '1' and '25' inclusive.")
+
+	if int(sys.argv[3]) not in [1,2]:
+		sys.exit("Level defined must be either '1' or '2'")
+except Exception as e:
+	sys.exit("Method not called properly. " +
+		"main.py [year (20xx)] [day (1-25)] [level (1..2)] expected.")
+
+year = str(sys.argv[1])
+day = str(sys.argv[2])
+level = str(sys.argv[3])
+
+start = datetime.datetime.now()
+print("Start execution for problem " + year + "-" + day + "-" + level + ".")
+
+print("Solution : " + str(adventofcode.solve(year, day, level)))
+
+print("Executed in " + str((datetime.datetime.now() - start).total_seconds()) + "s")
