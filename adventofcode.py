ed8bbe1c06cdcc0510bee11c1ceb1804a00e8a03
Valentin 'Ookamiko' Dewilde <valentin.dewilde@hotmail.be>
2024-12-02 08:51:54 +0100

    [FIX] Wrong use of if elif

--- adventofcode.py
+++ adventofcode.py
@@ -27,7 +27,7 @@ def get_input(year, day):
 
     if res.status_code == 404:
         sys.exit('No input found. Are you sure to ask for a year-day available ?')
-    else res.status_code != requests.codes.ok:
+    elif res.status_code != requests.codes.ok:
         sys.exit(res.text)
 
     return res.content.decode('UTF-8')
