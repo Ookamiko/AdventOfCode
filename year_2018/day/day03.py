c0ade53440866971cb2f5bf7392c277d856cfd56
Valentin 'Ookamiko' Dewilde <valentin.dewilde@hotmail.be>
2024-12-03 11:18:59 +0100

    [ADD] Resolve stage 2018-3-1 and 2018-3-2

--- year_2018/day/day03.py
+++ year_2018/day/day03.py
@@ -1,7 +1,52 @@
 #!/usr/bin/python
 
+import re as regex
+
 def level1(input):
-	return "Not implemented"
+	#input = '#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2\n'
+
+	table = []
+	for i in range(2000):
+		table.append([0] * 2000)
+
+	overlapping = 0
+
+	for line in input.strip().split('\n'):
+		s = [int(x) for x in regex.findall(r'(\d+)', line)]
+
+		for i in range(s[1], s[1] + s[3]):
+			for j in range(s[2], s[2] + s[4]):
+				if (table[i][j] == 1):
+					overlapping += 1
+				table[i][j] += 1
+
+	return overlapping
 
 def level2(input):
-	return "Not implemented"
+	#input = '#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2\n'
+
+	table = []
+	for i in range(2000):
+		table.append([0] * 2000)
+
+	not_overlapping = []
+
+	for line in input.strip().split('\n'):
+		s = [int(x) for x in regex.findall(r'(\d+)', line)]
+
+		not_overlapping.append(s[0])
+		overlap_with = []
+
+		for i in range(s[1], s[1] + s[3]):
+			for j in range(s[2], s[2] + s[4]):
+				if (table[i][j] != 0):
+					overlap_with.append(table[i][j])
+				table[i][j] = s[0]
+
+		if len(overlap_with) > 0:
+			not_overlapping.remove(s[0])
+			for i in overlap_with:
+				if i in not_overlapping:
+					not_overlapping.remove(i)
+
+	return not_overlapping[0]
