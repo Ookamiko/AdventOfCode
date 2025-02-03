0346ac46acccfa12daa4e75f4549fd91baa04713
Valentin 'Ookamiko' Dewilde <valentin.dewilde@hotmail.be>
2025-02-03 08:45:34 +0100

    [ADD] Resolve stage 2024-6-1 and 2024-6-2

--- year_2024/day/day06.py
+++ year_2024/day/day06.py
@@ -1,7 +1,38 @@
 #!/usr/bin/python
 
+import math
+
 def level1(input):
-	return "Not implemented"
+	#input = '....#.....\n.........#\n..........\n..#.......\n.......#..\n..........\n.#..^.....\n........#.\n#.........\n......#...\n'
+
+	maps = [x for x in input.replace('\n', '')]
+	size = int(math.sqrt(len(maps)))
+	move = -size
+	pos = maps.index('^')
+	maps[pos] = 'X'
+	valid = True
+
+	while valid:
+		if (pos + move < 0 or
+			pos + move >= len(maps) or
+			(move == -1 and pos % size == 0) or
+			(move == 1 and pos % size == size - 1)
+		):
+			valid = False
+		elif maps[pos + move] == '#':
+			if move == -1:
+				move = -size
+			elif move == 1:
+				move = size
+			elif move == size:
+				move = -1
+			else:
+				move = 1
+		else:
+			pos += move
+			maps[pos] = 'X'
+
+	return maps.count('X')
 
 def level2(input):
 	return "Not implemented"
