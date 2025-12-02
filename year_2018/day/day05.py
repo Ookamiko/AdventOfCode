f3edd3caa032532a6e1daca72743ade6b76bbb4f
Valentin 'Ookamiko' Dewilde <valentin.dewilde@hotmail.be>
2025-12-02 09:21:38 +0100

    [ADD] Resolve stage 2018-5-1 and 2018-5-2

--- year_2018/day/day05.py
+++ year_2018/day/day05.py
@@ -1,7 +1,50 @@
 #!/usr/bin/python
 
+import math
+
+def found_border(poly, low, up):
+	if low == 0 or up == len(poly) - 1:
+		return low, up
+
+	if poly[low - 1] != poly[up + 1] and poly[low - 1].lower() == poly[up + 1].lower():
+		return found_border(poly, low - 1, up + 1)
+	else:
+		return low, up
+
+def reduce_poly(poly):
+	i = 0
+
+	while i < len(poly) - 1:
+
+		if poly[i] != poly[i + 1] and poly[i].lower() == poly[i + 1].lower():
+			low, up = found_border(poly, i, i + 1)
+			poly = poly[:low] + poly[up+1:]
+			i = low
+		else:
+			i += 1
+
+	return poly
+
 def level1(input):
-	return "Not implemented"
+	#input = 'dabAcCaCBAcCcaDA\n'
+	poly = input.strip()
+
+	return len(reduce_poly(poly))
 
 def level2(input):
-	return "Not implemented"
+	#input = 'dabAcCaCBAcCcaDA\n'
+	units = ['a','z','e','r','t','y','u','i','o','p','q','s','d','f','g','h','j','k','l','m','w','x','c','v','b','n']
+	poly = input.strip()
+
+	best = math.inf
+
+	for unit in units:
+		if not(unit in poly or unit.upper() in poly):
+			continue
+
+		test_poly = poly.replace(unit, '').replace(unit.upper(), '')
+		r_poly = reduce_poly(test_poly)
+
+		best = min(best, len(r_poly))
+
+	return best
